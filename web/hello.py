4eae03ef616d2b366632800ffbba31f6a0abca3f
George Gabaraev <gvgeorge@mail.ru>
2020-08-12 15:15:13 +0300

    Fixed a bug within hello.py

--- web/hello.py
+++ web/hello.py
@@ -3,7 +3,7 @@ def wsgi_app(environ, start_response):
     if environ['REQUEST_METHOD'] != "GET":
      	raise NotImplementedError
     url_query_string = environ['QUERY_STRING']
-    newline_separated_query_string = [str.encode(q + '\n') for q in url_query_string].split('&')]
+    newline_separated_query_string = [str.encode(q + '\n') for q in url_query_string.split('&')]
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
