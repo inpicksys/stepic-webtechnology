5b70079c6ceae8c64671abb72f59ada5cf89e880
George Gabaraev <gvgeorge@mail.ru>
2020-08-13 14:22:41 +0300

    edited settings.py added app and allowed hosts

--- web/ask/ask/settings.py
+++ web/ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(wr7a#ajnjv_sr(&fd!*s+(g=#&^eec69)rb&e6duqf#rvz-gf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'qa'
 ]
 
 MIDDLEWARE = [
