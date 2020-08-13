9b1d909b7aa061bc9c7b206e4af8859619caa88d
George Gabaraev <gvgeorge@mail.ru>
2020-08-13 15:46:42 +0300

    settings bug fixed

--- web/ask/ask/settings.py
+++ web/ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(wr7a#ajnjv_sr(&fd!*s+(g=#&^eec69)rb&e6duqf#rvz-gf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
