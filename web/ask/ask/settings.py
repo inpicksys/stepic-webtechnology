1e6f396f8c128535c64ef68adf9812b760c23921
George Gabaraev <gvgeorge@mail.ru>
2020-08-15 18:59:36 +0300

    added new project ask and new app qa

--- web/ask/ask/settings.py
+++ web/ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vfdf75jfpzhc9h+js8oegfj#00%#!2uls+dt4x1%mf#xn$klb@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'qa'
 ]
 
 MIDDLEWARE = [
