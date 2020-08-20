a8ff40a3cb2570ecfc29611d88a5f1e18e9b4739
George Gabaraev <gvgeorge@mail.ru>
2020-08-20 18:21:20 +0300

    added question and answer models made migrations

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -1,3 +1,56 @@
-from django.db import models
+'''
+Question - вопрос
+title - заголовок вопроса
+text - полный текст вопроса
+added_at - дата добавления вопроса
+rating - рейтинг вопроса (число)
+author - автор вопроса
+likes - список пользователей, поставивших "лайк"
 
-# Create your models here.
+Answer - ответ
+text - текст ответа
+added_at - дата добавления ответа
+question - вопрос, к которому относится ответ
+author - автор ответа
+
+В качестве модели пользователя - используйте django.contrib.auth.models.User  из стандартной системы авторизации Django.
+
+5) Рядом с моделью Question определите менеджер реализующий следующие методы
+
+QuestionManager - менеджер модели Question
+new - метод возвращающий последние добавленные вопросы
+popular - метод возвращающий вопросы отсортированные по рейтингу
+'''
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='question_author')
+    likes = models.ManyToManyField(User, related_name='liked_by_user')
+    objects = QuestionManager()
+
+    def __str__(self):
+        return self.title
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
